845afa63376c695dfc03bceab2730b5c676b1bd1
mac <mac@192.168.0.100>
2017-07-09 16:23:40 +0800

    adapter pattern example

---
+++ AdapterPattern/src/adapter/example/Encryption.java
@@ -0,0 +1,11 @@
+package adapter.example;
+
+/**
+ * Created by mac on 09/07/2017.
+ */
+public class Encryption {
+    public String encryption(String content) {
+        System.out.println("encryption:" + content);
+        return content;
+    }
+}
